d53ce2fa99e31f53026d04bb7358bc36244e6bfe
lgaieta <lgaieta.dev@gmail.com>
2024-09-16 14:32:01 -0300

    Loaded students from database into ui

--- app/src/main/java/com/lgaieta/classmanager/students/ui/list/StudentsListScreen.kt
+++ app/src/main/java/com/lgaieta/classmanager/students/ui/list/StudentsListScreen.kt
@@ -1,10 +1,13 @@
 package com.lgaieta.classmanager.students.ui.list
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,6 +21,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.lgaieta.classmanager.R
+import com.lgaieta.classmanager.subjects.ui.list.SubjectItem
 import com.lgaieta.classmanager.ui.BottomNavBar
 import com.lgaieta.classmanager.ui.BottomNavBarActions
 import com.lgaieta.classmanager.ui.theme.HorizontalPagePadding
@@ -27,6 +31,7 @@ import com.lgaieta.classmanager.ui.theme.TopPagePadding
 fun StudentsListScreen(
     bottomNavBarActions: BottomNavBarActions,
     studentsListViewModel: StudentsListViewModel,
+    onStudentClick: (id: Int) -> Unit = {},
     modifier: Modifier = Modifier
 ) {
     val listState by studentsListViewModel.studentsListState.collectAsState()
@@ -51,6 +56,16 @@ fun StudentsListScreen(
                     textAlign = TextAlign.Center,
                 )
             }
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(listState.students) { student ->
+                    StudentItem(student, onClick = onStudentClick)
+                }
+            }
+            Spacer(modifier = Modifier.height(48.dp))
         }
     }
 }
