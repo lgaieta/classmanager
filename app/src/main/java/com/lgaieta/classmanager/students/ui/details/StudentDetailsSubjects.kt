4dabcc9ce2e1f22d493dc55fae3ed1c84770371b
lgaieta <lgaieta.dev@gmail.com>
2024-11-09 16:29:50 -0300

    Added empty subjects in StudentDetails

--- app/src/main/java/com/lgaieta/classmanager/students/ui/details/StudentDetailsSubjects.kt
+++ app/src/main/java/com/lgaieta/classmanager/students/ui/details/StudentDetailsSubjects.kt
@@ -1,15 +1,16 @@
 package com.lgaieta.classmanager.students.ui.details
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,10 +27,16 @@ fun StudentDetailsSubjects(subjects: List<Subject>) {
     Column {
         Text(
             text = stringResource(R.string.subjects),
-            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Bold),
+            style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
                 modifier = Modifier.align(Alignment.Start)
         )
         Spacer(modifier = Modifier.height(8.dp))
+        if (subjects.isEmpty()) {
+            Text(
+                text = stringResource(R.string.no_subjects),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
         if (subjects.isNotEmpty()) {
             LazyRow(
                 modifier = Modifier
@@ -38,10 +45,13 @@ fun StudentDetailsSubjects(subjects: List<Subject>) {
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 items(subjects) { subject ->
-                    Card {
+                    Card(
+                        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceContainerLow),
+                        border = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.surfaceContainerHigh)
+                    ) {
                         Text(
                             text = subject.name,
-                            style = MaterialTheme.typography.labelLarge,
+                            style = MaterialTheme.typography.bodyLarge,
                             modifier = Modifier
                                 .padding(24.dp)
                         )
